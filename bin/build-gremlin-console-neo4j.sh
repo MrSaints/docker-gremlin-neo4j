7d9681b570bf19ad7caeaee91afc6b720ebaec8b
Ian Lai <ian@fyianlai.com>
2016-09-06 20:51:40 +0100

    Add metadata labels to the Docker images

--- bin/build-gremlin-console-neo4j.sh
+++ bin/build-gremlin-console-neo4j.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker build --rm -f gremlin-console-neo4j/Dockerfile -t mrsaints/gremlin-console-neo4j ./gremlin-console-neo4j/
+docker build --rm \
+    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+    --build-arg VCS_REF=`git rev-parse --short HEAD` \
+    -f gremlin-console-neo4j/Dockerfile \
+    -t mrsaints/gremlin-console-neo4j \
+    ./gremlin-console-neo4j/
--- bin/build-gremlin-server-neo4j.sh
+++ bin/build-gremlin-server-neo4j.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker build --rm -f gremlin-server-neo4j/Dockerfile -t mrsaints/gremlin-server-neo4j ./gremlin-server-neo4j/
+docker build --rm \
+    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+    --build-arg VCS_REF=`git rev-parse --short HEAD` \
+    -f gremlin-server-neo4j/Dockerfile \
+    -t mrsaints/gremlin-server-neo4j \
+    ./gremlin-server-neo4j/
